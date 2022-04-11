e7accf62391c95c908f2a198e9c85be82b9ed247
visotoniki9 <d.simon.ssx0@gmail.com>
2022-04-11 22:22:23 +0200

    Added constants

---
+++ constants/assets.js
@@ -0,0 +1,41 @@
+import badge from '../assets/icons/badge.png';
+import eth from '../assets/icons/eth.png';
+import heart from '../assets/icons/heart.png';
+import left from '../assets/icons/left.png';
+import search from '../assets/icons/search.png';
+
+import logo from '../assets/images/logo.webp';
+import nft01 from '../assets/images/nft01.webp';
+import nft02 from '../assets/images/nft02.webp';
+import nft03 from '../assets/images/nft03.webp';
+import nft04 from '../assets/images/nft04.webp';
+import nft05 from '../assets/images/nft05.webp';
+import nft06 from '../assets/images/nft06.webp';
+import nft07 from '../assets/images/nft07.webp';
+
+import person01 from '../assets/images/person01.webp';
+import person02 from '../assets/images/person02.webp';
+import person03 from '../assets/images/person03.webp';
+import person04 from '../assets/images/person04.webp';
+
+export default {
+	badge,
+	eth,
+	heart,
+	left,
+	search,
+
+	logo,
+	nft01,
+	nft02,
+	nft03,
+	nft04,
+	nft05,
+	nft06,
+	nft07,
+
+	person01,
+	person02,
+	person03,
+	person04,
+};
